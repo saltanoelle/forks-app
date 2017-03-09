8a70c51ae02c93676f74449aba84521d50e95394
noelle <saltanoelle@gmail.com>
2017-03-08 18:44:06 -0800

    change routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "/" => "posts#index"
+  root to: "posts#index"
   get "/posts"=>"posts#index"
   get "/posts/new"=> "posts#new"
   post "/posts"=> "posts#create"
